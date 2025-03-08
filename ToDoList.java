973c37baa13f4d547d8f681d507854a3261490f3
jayson7 <lexxiijoo70@gmail.com>
2025-03-08 15:51:21 +0100

    todo list

---
+++ ToDoList.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ToDoList {
+    public static void main(String[] args) {
+        ArrayList<String> tasks = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\n--- To-Do List ---");
+            System.out.println("1. Add Task");
+            System.out.println("2. View Tasks");
+            System.out.println("3. Delete Task");
+            System.out.println("4. Exit");
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter a task: ");
+                    String task = scanner.nextLine();
+                    tasks.add(task);
+                    System.out.println("Task added!");
+                    break;
+                case 2:
+                    if (tasks.isEmpty()) {
+                        System.out.println("No tasks to display.");
+                    } else {
+                        System.out.println("Tasks:");
+                        for (int i = 0; i < tasks.size(); i++) {
+                            System.out.println((i + 1) + ". " + tasks.get(i));
+                        }
+                    }
+                    break;
+                case 3:
+                    if (tasks.isEmpty()) {
+                        System.out.println("No tasks to delete.");
+                    } else {
+                        System.out.print("Enter the task number to delete: ");
+                        int taskNumber = scanner.nextInt();
+                        if (taskNumber > 0 && taskNumber <= tasks.size()) {
+                            tasks.remove(taskNumber - 1);
+                            System.out.println("Task deleted!");
+                        } else {
+                            System.out.println("Invalid task number!");
+                        }
+                    }
+                    break;
+                case 4:
+                    System.out.println("Exiting...");
+                    return;
+                default:
+                    System.out.println("Invalid option!");
+            }
+        }
+    }
+}
