f1f26e71f0e5a8d32d6743c1ff703fed3537d320
jayson7 <lexxiijoo70@gmail.com>
2025-03-08 16:48:02 +0100

    todo list

---
+++ BankAccount.java
@@ -0,0 +1,39 @@
+public class BankAccount {
+    private String accountNumber; // Unique account number
+    private String accountHolderName; // Name of the account holder
+    private double balance; // Current balance
+
+    // Constructor to initialize the account
+    public BankAccount(String accountNumber, String accountHolderName, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = initialBalance;
+    }
+
+    // Method to deposit money
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Invalid deposit amount!");
+        }
+    }
+
+    // Method to withdraw money
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: $" + amount);
+        } else {
+            System.out.println("Invalid withdrawal amount or insufficient balance!");
+        }
+    }
+
+    // Method to check balance
+    public void checkBalance() {
+        System.out.println("Account Holder: " + accountHolderName);
+        System.out.println("Account Number: " + accountNumber);
+        System.out.println("Current Balance: $" + balance);
+    }
+}
